ed825cee054b70cdb57a80aa2dc5c6db27378799
nicomanunta <nic.manun@gmail.com>
2024-01-29 14:53:41 +0100

    Scritto file index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>    
+        <h1 class="text-center mt-3 fw-bold">Compiti di oggi!</h1>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <ul class="list-group list-group-flush my-5 w-50">
+                        <li class="list-group-item d-flex" >
+                            <div>
+                                <span>
+                                    {{}}
+                                </span>
+                            </div>
+                        </li>   
+                    </ul>    
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="./js/script.js" type="text/javascript"></script>
+</body>
+</html>
